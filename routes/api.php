340c68b89675f2acad5b68f7e396eca5617d9cea
natsu <natsu@USERnoMacBook-Air.local>
2026-02-12 04:49:59 +0900

    add: admin権限ミドルウェアをルートに適用

--- routes/api.php
+++ routes/api.php
@@ -10,42 +10,43 @@ use App\Http\Controllers\PinHistoryController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DailyScheduleController;
 
-Route::post('/login', [AuthController::class, 'login']);
-
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
 
-     // セル
-    Route::get('/damage-cells', [DamageCellController::class, 'index']);
-    Route::post('/damage-cells', [DamageCellController::class, 'store']);
-    Route::delete('/damage-cells/{id}', [DamageCellController::class, 'destroy']);
-
-    Route::get('/ban-cells', [BanCellController::class, 'index']);
-    Route::post('/ban-cells', [BanCellController::class, 'store']);
-    Route::delete('/ban-cells/{id}', [BanCellController::class, 'destroy']);
-
-    Route::get('/rain-cells', [RainCellController::class, 'index']);
-    Route::post('/rain-cells', [RainCellController::class, 'store']);
-    Route::delete('/rain-cells/{id}', [RainCellController::class, 'destroy']);
-
-    // ピン
+    // ピン（全員）
     Route::get('/pins', [PinController::class, 'index']);
     Route::post('/pins', [PinController::class, 'store']);
     Route::delete('/pins/{id}', [PinController::class, 'destroy']);
 
-    // ピン履歴
+    // ピン履歴（全員）
     Route::get('/pin-histories', [PinHistoryController::class, 'index']);
 
-    // ユーザー管理
-    Route::get('/users', [UserController::class, 'index']);
-    Route::post('/users', [UserController::class, 'store']);
-    Route::put('/users/{id}', [UserController::class, 'update']);
-    Route::delete('/users/{id}', [UserController::class, 'destroy']);
-
-    // スケジュール
+    // セル・スケジュール（参照は全員）
+    Route::get('/damage-cells', [DamageCellController::class, 'index']);
+    Route::get('/ban-cells', [BanCellController::class, 'index']);
+    Route::get('/rain-cells', [RainCellController::class, 'index']);
     Route::get('/schedules', [DailyScheduleController::class, 'index']);
-    Route::post('/schedules', [DailyScheduleController::class, 'store']);
-    Route::put('/schedules/{id}', [DailyScheduleController::class, 'update']);
-    Route::delete('/schedules/{id}', [DailyScheduleController::class, 'destroy']);
+
+    // admin専用
+    Route::middleware('admin')->group(function () {
+        // ユーザー管理
+        Route::get('/users', [UserController::class, 'index']);
+        Route::post('/users', [UserController::class, 'store']);
+        Route::put('/users/{id}', [UserController::class, 'update']);
+        Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+        // セル編集
+        Route::post('/damage-cells', [DamageCellController::class, 'store']);
+        Route::delete('/damage-cells/{id}', [DamageCellController::class, 'destroy']);
+        Route::post('/ban-cells', [BanCellController::class, 'store']);
+        Route::delete('/ban-cells/{id}', [BanCellController::class, 'destroy']);
+        Route::post('/rain-cells', [RainCellController::class, 'store']);
+        Route::delete('/rain-cells/{id}', [RainCellController::class, 'destroy']);
+
+        // スケジュール編集
+        Route::post('/schedules', [DailyScheduleController::class, 'store']);
+        Route::put('/schedules/{id}', [DailyScheduleController::class, 'update']);
+        Route::delete('/schedules/{id}', [DailyScheduleController::class, 'destroy']);
+    });
 });
