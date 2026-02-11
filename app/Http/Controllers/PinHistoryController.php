a2a2ed725899637218942b6d73428ce4331f6d8e
natsu <natsu@USERnoMacBook-Air.local>
2026-02-11 19:47:26 +0900

    add: PinHistoryController

--- app/Http/Controllers/PinHistoryController.php
+++ app/Http/Controllers/PinHistoryController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PinHistory;
 use Illuminate\Http\Request;
 
 class PinHistoryController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $query = PinHistory::query();
+
+        if ($request->hole_number) {
+            $query->where('hole_number', $request->hole_number);
+        }
+
+        if ($request->date) {
+            $query->where('date', $request->date);
+        }
+
+        $histories = $query->orderBy('date', 'desc')->get();
+        return response()->json($histories);
+    }
 }
