feaefb77e6d313cb7e20e56ff6f91516f140d4d7
natsu <natsu@USERnoMacBook-Air.local>
2026-02-18 06:52:46 +0900

    refactor: Pin, User系FormRequest作成（Store, Update）

--- app/Http/Controllers/PinController.php
+++ app/Http/Controllers/PinController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePinRequest;
 use App\Models\Pin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,20 +14,16 @@ class PinController extends Controller
         $pins = Pin::with('creator')
             ->where('hole_number', $request->hole_number)
             ->get();
+
         return response()->json($pins);
     }
 
-    public function store(Request $request)
+    public function store(StorePinRequest $request)
     {
-        $validated = $request->validate([
-            'hole_number' => 'required|integer|between:1,18',
-            'x' => 'required|integer',
-            'y' => 'required|integer',
-        ]);
-
+        $validated = $request->validated();
         $validated['created_by'] = Auth::id();
-
         $pin = Pin::create($validated);
+
         return response()->json($pin, 201);
     }
 
@@ -34,6 +31,7 @@ class PinController extends Controller
     {
         $pin = Pin::findOrFail($id);
         $pin->delete();
+
         return response()->json(['message' => '削除しました']);
     }
 }
