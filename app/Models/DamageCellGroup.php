bb49d8acdd01c250cda549b6dc138b3b0461f22c
natsu <natsu@USERnoMacBook-Air.local>
2026-02-22 03:56:13 +0900

    fix: DamageCellGroup空ファイル修正

---
+++ app/Models/DamageCellGroup.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class DamageCellGroup extends Model
+{
+    use HasUuids;
+
+    const UPDATED_AT = null;
+
+    protected $fillable = [
+        'hole_number',
+        'comment',
+    ];
+
+    public function cells(): HasMany
+    {
+        return $this->hasMany(DamageCell::class, 'group_id');
+    }
+}
