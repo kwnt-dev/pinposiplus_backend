a8de14f31d175d88f6ff35fd481ec9d6c12b9534
kwnt-dev <kwnt.dev@gmail.com>
2026-03-15 13:00:44 +0900

    fix: CORS設定に本番URLを追加

--- config/cors.php
+++ config/cors.php
@@ -19,8 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
-
+    'allowed_origins' => ['http://localhost:3000', 'https://pinposiplus.com'],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
@@ -29,7 +28,6 @@ return [
 
     'max_age' => 0,
 
-  'supports_credentials' => true,
-
+    'supports_credentials' => true,
 
 ];
